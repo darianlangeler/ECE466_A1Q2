f5885f42511084a4fdc1d33dafabcc66650be347
darianlangeler <48569079+darianlangeler@users.noreply.github.com>
2021-07-19 23:34:45 -0700

    undo misupload

--- adder.cpp
+++
@@ -1,7 +0,0 @@
-#include "systemc.h"
-#include "modules.h"
-
-void adder::add_process() {
-
-	sum.write(summandA.read() + summandB.read());
-}
--- multiplier.cpp
+++
@@ -1,7 +0,0 @@
-#include "systemc.h"
-#include "modules.h"
-
-void multiplier::multiply_process() {
-
-	product.write(multiplicandA.read() * multiplicandB.read());
-}
