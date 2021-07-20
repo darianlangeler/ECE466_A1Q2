02490f771e1cb1428d600692a2001904c58fe24b
darianlangeler <48569079+darianlangeler@users.noreply.github.com>
2021-07-20 12:26:50 -0700

    Revert "undo misupload"
    This reverts commit f5885f42511084a4fdc1d33dafabcc66650be347.

---
+++ stim.h
@@ -0,0 +1,24 @@
+#include "systemc.h"
+#include <iomanip>
+
+SC_MODULE(stim)
+{
+  sc_out<sc_logic> reset;
+  sc_out<float> x;
+  sc_in_clk Clk;
+
+  void stimgen()
+  {
+    reset.write(SC_LOGIC_1);
+    wait();
+    reset.write(SC_LOGIC_0);
+    x.write(1);
+    wait();
+    x.write(0);
+    //sc_stop();
+  }
+  SC_CTOR(stim)
+  {
+    SC_CTHREAD(stimgen, Clk.pos());
+  }
+};
